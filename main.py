284cea0209c3a1d7f927b7cb37190eb6b05f6b55
Zhalo6 <99208362+Zhalo6@users.noreply.github.com>
2024-03-27 15:34:11 +0000

    Displayed a hitbox on screen, updates word when correct/incorrect word is typed.

--- main.py
+++ main.py
@@ -1,28 +1,51 @@
-import pygame
+import pygame, sys
+from pygame.locals import QUIT
 from randomWordGen import *
 
 pygame.init()
 screen = pygame.display.set_mode((400, 300))
 pygame.display.set_caption('Monster Hunter')
-     
+
+class Hitbox(object):
+  def __init__(self, rect_x, rect_y, rect_width, rect_height, text):
+    self.rect_x = rect_x
+    self.rect_y = rect_y
+    self.rect_width = rect_width
+    self.rect_height = rect_height   
+    self.text = text   
+    self.font = pygame.freetype.SysFont('Arial', 25)
+
+  def draw(self):
+    pygame.draw.rect(screen, (200, 200, 200), 
+        (self.rect_x, self.rect_y, self.rect_width, self.rect_height))     
+    self.font.render_to(screen, (self.rect_x, self.rect_y), self.text, (0, 0, 0))
+
 def main():
-
   currentWord = generateWord()
-  print(currentWord)
+  running = True
+  while running:
+      screen.fill('blue')
+      head = Hitbox(10, 10, 25 + (10 * len(currentWord)), 50, currentWord)
+      head.draw()
+      pygame.display.flip()
+      
+      print(currentWord)
+      
+      inputText = input()
 
-  inputText = input()
+      if inputText == currentWord:
+        currentWord = ''
+        print("Correct!")
+        currentWord = generateWord()
+        
+      elif inputText != currentWord:
+        currentWord =''
+        print("Loser")
+        currentWord = generateWord()
+   
 
-  if inputText == currentWord:
-    currentWord = ''
-    print("Correct!")
 
-  if inputText != currentWord:
-    currentWord =''
-    print("Loser")
-    
-           
-  pygame.display.update()
+  
 
 if __name__ == "__main__":
- while True:
     main()
